e78a9920c22c8b79888db9d883267db2cae3b44b
fdl1124 <fadil20242011@gmail.com>
2026-08-24 03:44:05 +0100

    Rendre AuthModule global pour la resolution des guards dans tous les modules

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,8 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { FirebaseService } from './firebase.service';
 import { FirebaseAuthGuard } from './firebase-auth.guard';
 
+@Global()
 @Module({
   providers: [FirebaseService, FirebaseAuthGuard],
   exports: [FirebaseService, FirebaseAuthGuard],
